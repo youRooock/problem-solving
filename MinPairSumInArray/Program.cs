126293812f2935ef380c24ef0d6629213fca8ad8
Yurii Khmelenko <junioriks@rambler.ru>
2019-09-27 00:43:44 +0300

    added 'find min pair sum' task

---
+++ MinPairSumInArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MinPairSumInArray
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var arr = new[] {3, 5, 6, 2};
+
+      Console.Write(FindMinPairSum(arr));
+    }
+
+    static int FindMinPairSum(int[] arr)
+    {
+      int min1 = Int32.MaxValue;
+      int min2 = Int32.MaxValue;
+
+      foreach (var a in arr)
+      {
+        if (a <= min1)
+        {
+          min2 = min1;
+          min1 = a;
+        }
+
+        if (min2 > a && a != min1)
+        {
+          min2 = a;
+        }
+      }
+
+      return min1 + min2;
+    }
+  }
+}
---
+++ MinPairSumInArray/README.md
@@ -0,0 +1,2 @@
+### Given an array of distinct integers arr[], the task is to find a pair which has the minimum sum and print the sum.
+`Example: Input:  arr[] = {1, 2, 3}, Output: 3`
