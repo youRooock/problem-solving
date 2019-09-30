d83b9eb3f3cd83baa55f42ed5ee523b5ac7e7e2e
Yurii Khmelenko <junioriks@rambler.ru>
2019-10-01 00:59:31 +0300

    added is given string palindrome task

---
+++ IsGivenStringPalindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace IsGivenStringPalindrome
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string testStr1 = "abba";
+      string testStr2 = "abbc";
+      string testStr3 = "Was it a cat I saw";
+
+      Console.WriteLine(IsPalindrome(testStr1));
+      Console.WriteLine(IsPalindrome(testStr2));
+      Console.WriteLine(IsPalindrome(testStr3));
+    }
+
+    static bool IsPalindrome(string s)
+    {
+      var arr = s.Replace(" ", "").ToLower().ToCharArray();
+
+      for (int i = 0, j = arr.Length - 1; j > i; i++, j--)
+      {
+        if (arr[i] != arr[j]) return false;
+      }
+
+      return true;
+    }
+  }
+}
---
+++ IsGivenStringPalindrome/README.md
@@ -0,0 +1,2 @@
+### Given a string, write a c function to check if it is palindrome or not. 
+     For example, “abba” is palindrome, but “abbc” is not palindrome.
